b33e6eba9f2de809b4f74d4e16482603f0ae2b70
Eric <me@bostrot.com>
2021-08-12 14:07:14 +0200

    Fixed dockerfile path

--- build_docker.sh
+++ build_docker.sh
@@ -2,7 +2,9 @@
 # Simple script to build containers and upload to registry
 
 # Login to docker hub
-#docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD
+docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD
+
+docker buildx create --use
 
 # Build and push docker containers
 for folder in $(ls -d */);
@@ -16,5 +18,5 @@ do
     echo "Building and pushing $folder_name";
     # echo `docker build -t bostrot/$folder_name:latest $folder`;
     # echo `docker push bostrot/$folder_name:latest`;
-    echo `docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag bostrot/$folder_name:latest .`
+    echo `docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag bostrot/$folder_name:latest $folder`
 done;
