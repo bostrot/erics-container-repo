663f87c81d7b1addc0d97c8415e07a2800682172
Eric <me@bostrot.com>
2021-08-28 22:05:03 +0200

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -7,7 +7,7 @@
 #docker context create ctx
 #docker buildx create --use
 
-docker login --password-stdin -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD
+docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --name multiarch --driver docker-container --use
 docker buildx inspect --bootstrap
