d3be32a52cad57fbb6b0b4125d4d3f06fa15ec56
Eric <me@bostrot.com>
2021-08-29 10:23:08 +0200

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -2,12 +2,8 @@
 # Simple script to build containers and upload to registry
 
 # Login to docker hub
-# docker login --password-stdin -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD
-
-#docker context create ctx
-#docker buildx create --use
-
 docker login --username $CI_REGISTRY_USER --password $CI_REGISTRY_PASSWORD
+
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --name multiarch --driver docker-container --use
 docker buildx inspect --bootstrap
@@ -22,7 +18,5 @@ do
         continue
     fi
     echo "Building and pushing $folder_name";
-    # echo `docker build -t bostrot/$folder_name:latest $folder`;
-    # echo `docker push bostrot/$folder_name:latest`;
-    echo `docker buildx build --push --platform linux/amd64,linux/arm64,linux/riscv64,linux/ppc64le,linux/386,linux/arm/v7 --tag bostrot/$folder_name:latest --tag bostrot/$folder_name:latest $folder`
+    echo `docker buildx build --push --platform linux/amd64,linux/arm/v7,linux/arm64,linux/386,linux/ppc64le,linux/riscv64,linux/s390x --tag bostrot/$folder_name:latest $folder`
 done;
